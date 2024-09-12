fa8faf4571f91843c5f5c4007c845b8680f93b78
SIM15 <SIM15@172.23.48.26>
2024-09-12 13:47:00 -0600

    Edges were created

--- src/main/java/com/mycompany/grafo01/Grafo01.java
+++ src/main/java/com/mycompany/grafo01/Grafo01.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.grafo01;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author SIM15
@@ -12,6 +14,23 @@ public class Grafo01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hola");
+        Vertice01 v1 = new Vertice01();
+        v1.setLabel("Uno");
+        v1.setData(1);
+        v1.setEdges(new ArrayList<>());
+        
+        Vertice01 v2 = new Vertice01();
+        v2.setLabel("Dos");
+        v2.setData(2);
+        v2.setEdges(new ArrayList<>());
+
+        Vertice01 v3 = new Vertice01();
+        v3.setLabel("Tres");
+        v3.setData(3);
+        v3.setEdges(new ArrayList<>());
+        
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
     }
 }
