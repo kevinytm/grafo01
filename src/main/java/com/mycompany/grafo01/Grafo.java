735ae990fe97bd9cdf99764004a392db9e525b42
SIM15 <SIM15@172.23.48.26>
2024-09-13 16:06:43 -0600

    Grafos

---
+++ src/main/java/com/mycompany/grafo01/Grafo.java
@@ -0,0 +1,47 @@
+
+package com.mycompany.grafo01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author SIM15
+ */
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Grafo {
+    private String name;
+    private List<Vertice01> vertices;
+    
+    public Grafo(String name){
+        this.name = name;
+        this.vertices = new ArrayList<>();
+    }
+    
+    public void addVertice(Vertice01 vertice){
+        vertices.add(vertice);
+    }
+    
+    public void addVertice(Vertice01... vertice){
+        /*for(Vertice01 v:vertices){
+            this.vertices.add(v);
+        }*/
+        this.vertices.addAll(Arrays.asList(vertice));
+
+    }
+    
+    @Override
+    public String toString(){
+        return name+" [Grado: "+vertices.size()+"]";
+    }
+}
--- src/main/java/com/mycompany/grafo01/Grafo01.java
+++ src/main/java/com/mycompany/grafo01/Grafo01.java
@@ -14,23 +14,37 @@ public class Grafo01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Vertice01 v1 = new Vertice01();
-        v1.setLabel("Uno");
-        v1.setData(1);
-        v1.setEdges(new ArrayList<>());
         
-        Vertice01 v2 = new Vertice01();
-        v2.setLabel("Dos");
-        v2.setData(2);
-        v2.setEdges(new ArrayList<>());
-
-        Vertice01 v3 = new Vertice01();
-        v3.setLabel("Tres");
-        v3.setData(3);
-        v3.setEdges(new ArrayList<>());
+        Vertice01 v1 = new Vertice01("Uno",1);
+        Vertice01 v2 = new Vertice01("Dos",2);
+        Vertice01 v3 = new Vertice01("Tres",3);
+        Vertice01 v4 = new Vertice01("Cuatro",4);
+        
+        /*Creación de aristas*/
+        
+        v1.getEdges().add(v3);
+        v1.getEdges().add(v4);
+        v1.getEdges().add(v2);
+        
+        v2.getEdges().add(v4);
+        v3.getEdges().add(v1);
+        v4.getEdges().add(v3);
+        
+        Grafo grafo01=new Grafo();
+        grafo01.addVertice(v1,v2,v3,v4);
         
         System.out.println(v1);
         System.out.println(v2);
         System.out.println(v3);
+        System.out.println(v4);
+        
+        
+        
+        //Hace referencia al mismo valor v3
+        /*Vertice01 verticeAuxiliar = v3;
+        System.out.println(v3);
+        System.out.println(verticeAuxiliar);
+        verticeAuxiliar.setData(345);
+        System.out.println(verticeAuxiliar);*/
     }
 }
