0189836a4cdbeba731de4538f4ea684c82d60591
kevin <kevin@kevin>
2024-09-19 14:44:47 -0600

    Recorrido

--- src/main/java/com/mycompany/grafo01/Vertice01.java
+++ src/main/java/com/mycompany/grafo01/Vertice01.java
@@ -5,6 +5,7 @@
 package com.mycompany.grafo01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,11 +21,16 @@ public class Vertice01 {
     private String label;
     private int data;
     private List<Vertice01> edges;
+    private boolean visited;
     
     public Vertice01(String label,int data){
         this.label = label;
         this.data=data;
         this.edges = new ArrayList<>();
+    }
+ 
+    public void addEdges(Vertice01... vertice){
+        this.edges.addAll(Arrays.asList(vertice));
     }
     
     @Override
